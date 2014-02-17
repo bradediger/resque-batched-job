8beeb8a4ce24e475713fdaa28cb5c2d8243f72e0
Brad Ediger <brad.ediger@madriska.com>
2014-02-17 15:33:04 -0600

    Remove Resque::Helpers

--- lib/resque/plugins/batched_job.rb
+++ lib/resque/plugins/batched_job.rb
@@ -1,29 +1,27 @@
 module Resque
-  
+
   module Plugin
-    
+
     #
     # This hook is really the meaning of our adventure.
     def after_batch_hooks(job)
       job.methods.grep(/^after_batch/).sort
     end
-    
-  end
-  
-  module Plugins
-    
-    module BatchedJob
-    
-      include Resque::Helpers
 
-      # 
+  end
+
+  module Plugins
+
+    module BatchedJob
+
+      #
       #   Like enqueue(), but enqueues an entire batch at a time. This ensures
       # that if the first job finishes before any other jobs are enqueued, the
       # first job doesn't think it was the last one and run the finalize hooks.
       def enqueue_batch(batch_id, arglist)
         # Add everything to the batch set first, so we know it won't be empty
         # until we've finished everything in the batch.
-        arglist.each { |args| redis.sadd(batch(batch_id), encode(args)) }
+        arglist.each { |args| Resque.redis.sadd(batch(batch_id), Resque.encode(args)) }
 
         # Enqueue the jobs.
         arglist.each { |args| Resque.enqueue(self, batch_id, *args) }
@@ -38,28 +36,28 @@ module Resque
       #
       # Batch the job.  The first argument of a batched job, is the batch id.
       def after_enqueue_batch(id, *args)
-        redis.sadd(batch(id), encode(args))
+        Resque.redis.sadd(batch(id), Resque.encode(args))
       end
-      
+
 =begin
-  TODO: Determine if it's necessary to double check the jobs existance 
+  TODO: Determine if it's necessary to double check the jobs existance
     before performing it.  If so, do what?
-    
+
       def before_perform_audit_batch(id, *args)
-        unless redis.sismember(batch(id), "#{encode(args)}")
+        unless Resque.redis.sismember(batch(id), "#{Resque.encode(args)}")
           raise Resque::Job::DontPerform.new("#{args} are not a member of #{batch(id)}")
         end
       end
 =end
 
-      # 
+      #
       #   After every job, no matter in the event of success or failure, we need
       # to remove the job from the batch set.
       def around_perform_amend_batch(id, *args)
         begin
           yield
         ensure
-          redis.srem(batch(id), "#{encode(args)}")
+          Resque.redis.srem(batch(id), "#{Resque.encode(args)}")
         end
       end
 
@@ -74,18 +72,18 @@ module Resque
           end
         end
       end
-      
+
       #
-      #   Checks to see if the batch key exists.  If the key does exist, is the 
-      # set empty?  The Redis srem command deletes the key when the last item 
+      #   Checks to see if the batch key exists.  If the key does exist, is the
+      # set empty?  The Redis srem command deletes the key when the last item
       # of a set is removed. Ah, go ahead and check the size.
       def batch_complete?(id)
-        redis.scard(batch(id)) == 0
+        Resque.redis.scard(batch(id)) == 0
       end
       alias :batch_exist? :batch_complete? # => are they?
-      
+
     end
-    
+
   end
-  
+
 end
